e290c509d28e8769165e0321e3cdf594fa56a958
mrh000mrh <82106801+mrh000mrh@users.noreply.github.com>
2026-02-18 22:12:13 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    ADMIN_ID = int(os.getenv("ADMIN_ID") or 0)
+    BRAND_NAME = os.getenv("BRAND_NAME", "NONEcore")
+    CHANNELS = os.getenv("CHANNELS", "@nonecorebot").split(",")
+    DATABASE_PATH = os.getenv("DATABASE_PATH", "database.db")
