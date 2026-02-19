9a29d1213f6e0d5f6cf88b97c5feb89980796e3a
mrh000mrh <82106801+mrh000mrh@users.noreply.github.com>
2026-02-20 04:04:34 +0530

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -3,8 +3,8 @@ from telegram import ReplyKeyboardMarkup, KeyboardButton
 class Keyboard:
     def main_menu(self):
         keyboard = [
-            [KeyboardButton("📤 آپلود HTML"), KeyboardButton("📤 ارسال دستی")],
+            [KeyboardButton("📤 آپلود HTML یا متن لینک"), KeyboardButton("📤 ارسال دستی")],
             [KeyboardButton("📊 آمار"), KeyboardButton("📤 ارسال پیام به کانال")],
-            [KeyboardButton("بازگشت به منو")]
+            [KeyboardButton("🗑️ پاک کردن تکراری‌ها")]
         ]
         return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
