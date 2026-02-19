1e090ba575c1fbdc36ea2445668392784a71ee78
mrh000mrh <82106801+mrh000mrh@users.noreply.github.com>
2026-02-20 04:01:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,5 @@ class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     ADMIN_ID = int(os.getenv("ADMIN_ID") or 0)
     BRAND_NAME = os.getenv("BRAND_NAME", "NONEcore")
-    CHANNELS = os.getenv("CHANNELS", "@nonecorebot").split(",")
+    CHANNELS = [c.strip() for c in os.getenv("CHANNELS", "@nonecorebot").split(",") if c.strip()]
     DATABASE_PATH = os.getenv("DATABASE_PATH", "database.db")
-
-    ALLOWED_CLIENTS = ["v2rayNG", "Streisand", "Hiddify"]
